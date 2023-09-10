d0b468ea7b51e95e8d8d8be0523cac9fb61316c7
Jared Chesebro <chesebrojared@gmail.com>
2023-09-10 01:15:47 -0700

    first commit, testing react, one component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+    <Header> </Header>
+    <div>
+      <h1>This is a test</h1>
+      <p>I need to remember how react works</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
